55d53c1dfdf31f15757bca1472393bf2d8cf24ee
DonnyVerduijn <donnyverduijn@gmail.com>
2018-06-12 17:55:06 +0200

    add tests with chromeHeadless, karma, mocha, chai

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
-    "extends": "airbnb-base",
-    "env": {
-        "browser": true
-    }
+  extends: "airbnb-base",
+  env: {
+    browser: true,
+    mocha: true
+  },
+  plugins: ["mocha", "chai-friendly"],
+  rules: {
+    "mocha/no-exclusive-tests": "error"
+  }
 };
