798430236c34e0364893c219b8d0d76fab3019d8
DonnyVerduijn <donnyverduijn@gmail.com>
2018-06-15 21:01:44 +0200

    update formatting

--- src/utils/isHTMLElement.js
+++ src/utils/isHTMLElement.js
@@ -1,8 +1,8 @@
 export default o =>
-  typeof document.HTMLElement === 'object'
+  (typeof document.HTMLElement === 'object'
     ? o instanceof document.HTMLElement // DOM2
     : o &&
       typeof o === 'object' &&
       o !== null &&
       o.nodeType === 1 &&
-      typeof o.nodeName === 'string';
+      typeof o.nodeName === 'string');
